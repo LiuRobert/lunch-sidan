ef94ab674353ac48c2eae5e7ef35fc92bac35df1
Robert <robert.ho89@gmail.com>
2022-09-25 19:19:18 +0200

    First Hotel

--- cron.py
+++ cron.py
@@ -1,6 +1,7 @@
 import scrapers.brandstation
 import scrapers.bryners
 import scrapers.estreet
+import scrapers.firsthotel
 import scrapers.invito
 import scrapers.qbar
 import json
@@ -10,6 +11,7 @@ scraper_list = []
 scraper_list.append(scrapers.brandstation.Scraper())
 scraper_list.append(scrapers.bryners.Scraper())
 scraper_list.append(scrapers.estreet.Scraper())
+scraper_list.append(scrapers.firsthotel.Scraper())
 scraper_list.append(scrapers.invito.Scraper())
 scraper_list.append(scrapers.qbar.Scraper())
 
